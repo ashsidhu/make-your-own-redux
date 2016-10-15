73de17765db3b329c7c73c3a6c92dd4db66a2804
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2016-10-15 18:55:33 +0530

    add react, react-redux; run basic todo app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import './index.css';
+import todoApp from './reducers'
+
+// these will be implemented by us
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+let store = createStore(todoApp)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
